9defa44b339dec54114df96027b892715529d960
Gustavo Quim <gquimb@miumg.edu.gt>
2024-05-31 15:40:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Gestor de Tareas
+
+Este proyecto es un simple gestro de tareas desarrollado en javaScript, diseñado para permitir a los usuarios agregar, completar y eliminar tareas.
+
+## Estructura del proyecto
+
+El proyecto está organizado de la siguiente manera:
+
+- `index.html`: Contiene la estructura básica de la página web con el formulario para agregar tareas y la lista de tareas.
+- `style.css`: Archivo de estilos para daar fomato a la página.
+- `src/`
+    - `task.ts`: Clase TypeScript para representar una tarea.
+    - `main.ts`: Cóodigo principal TypeScript que maneja la lógica de la aplicación.
+
+## Uso
+Una vez que el proyecto este funcionando en el navegador, el usuario puede:
+
+- Agregar nuevas tareas utilizando el formulario proporcionado.
+- Marcar las tareas como completadas haciendo click sobre la tarea correspondiente.
+- Eliminar tareas haciendo clic en el botón de eliminar junto a cada tarea.
---
+++ src/main.js
@@ -0,0 +1,58 @@
+import { Tarea } from "./task.js";
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+let tareas=[];
+
+function addTask(descripcion){
+    const newTask = new Tarea(descripcion);
+    tareas.push(newTask);
+    renderTask();
+}
+
+function renderTask(){
+    taskList.innerHTML = '';
+    tareas.forEach(tarea =>{
+        const li = document.createElement('li')
+        li.textContent = tarea.descripcion;
+        //taskList.appendChild(li);desde la
+        
+        //boton para eliminar tareas
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Eliminar";
+        deleteBtn.classList.add('eliminar-btn');
+        deleteBtn.addEventListener('click',()=>{
+            deleteTask(tarea);
+        })
+
+
+        if(tarea.completado){
+            li.classList.add('completada');
+        }
+        //a
+        li.addEventListener('click',()=>{
+            tarea.tareaCompletada();
+            renderTask();
+        });
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    })
+
+}
+
+taskForm.addEventListener('submit',event =>{
+    event.preventDefault();
+    const descripcion = taskInput.value.trim();
+    if (descripcion !== ''){
+        addTask(descripcion);
+        taskInput.value = "";
+    }
+})
+//funcion para eliminar tareas
+function deleteTask(taskDelete){
+    tareas = tareas.filter(tarea => tarea !== taskDelete);
+    renderTask();
+}
+
+renderTask();
